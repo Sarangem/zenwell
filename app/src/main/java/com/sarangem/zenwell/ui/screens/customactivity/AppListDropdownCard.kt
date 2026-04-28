de6e099c9347074f9bd8dd93fefdcb432862c55b
sarangem <palproneel@gmail.com>
2026-04-28 18:29:11 +0530

    Make "Add custom views" screen adaptive to smaller width screens

---
+++ app/src/main/java/com/sarangem/zenwell/ui/screens/customactivity/AppListDropdownCard.kt
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2026 Proneel Pal <palproneel@gmail.com>
+ * Licensed under the GNU General Public License v3.0 or later.
+ */
+
+package com.sarangem.zenwell.ui.screens.customactivity
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import coil3.compose.AsyncImage
+import com.sarangem.zenwell.R
+import com.sarangem.zenwell.utils.PackageInfo
+import kotlin.collections.forEach
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppListDropdownCard(
+    currentApp: String,
+    onCurrentAppChange: (String) -> Unit,
+    installedAppsList: List<PackageInfo>,
+) {
+    var expanded by remember { mutableStateOf(false) }
+    val currentApp = installedAppsList.firstOrNull { it.packageName == currentApp }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+    ) {
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable),
+            readOnly = true,
+            value = TextFieldValue(currentApp?.appName ?: stringResource(R.string.select_app)),
+            onValueChange = {},
+            leadingIcon = {
+                AsyncImage(
+                    currentApp?.icon,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(dimensionResource(R.dimen.padding_small))
+                        .clip(RoundedCornerShape(dimensionResource(R.dimen.padding_small)))
+                        .size(dimensionResource(R.dimen.image_size))
+                )
+            },
+            trailingIcon = @Composable {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            },
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = !expanded },
+            shape = MaterialTheme.shapes.large
+        ) {
+            installedAppsList.forEach {
+                DropdownMenuItem(
+                    text = {
+                        Text(
+                            it.appName,
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    },
+                    onClick = {
+                        onCurrentAppChange(it.packageName)
+                        expanded = !expanded
+                    },
+                    leadingIcon = {
+                        AsyncImage(
+                            it.icon,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .padding(dimensionResource(R.dimen.padding_small))
+                                .clip(RoundedCornerShape(dimensionResource(R.dimen.padding_small)))
+                                .size(dimensionResource(R.dimen.image_size))
+                        )
+                    },
+                )
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/sarangem/zenwell/ui/screens/customactivity/SaveAndDeleteButton.kt
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2026 Proneel Pal <palproneel@gmail.com>
+ * Licensed under the GNU General Public License v3.0 or later.
+ */
+
+package com.sarangem.zenwell.ui.screens.customactivity
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.sarangem.zenwell.R
+import com.sarangem.zenwell.ui.screens.edit.fields.ShowConfirmDialog
+import com.sarangem.zenwell.ui.theme.Green500
+
+@Composable
+fun SaveAndDeleteButton(
+    isSaved: Boolean,
+    onSave: () -> Unit,
+    onDelete: () -> Unit
+){
+    var showDeleteDialog by remember { mutableStateOf(false) }
+    if (showDeleteDialog) {
+        ShowConfirmDialog(
+            icon = R.drawable.filled_delete,
+            title = stringResource(R.string.delete),
+            description = stringResource(R.string.delete_confirmation_custom_view),
+            onDismiss = { showDeleteDialog = false },
+            onConfirm = {
+                showDeleteDialog = false
+                onDelete()
+            }
+        )
+    }
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(
+            onClick = { if (!isSaved) onSave() },
+            shape = CircleShape,
+            colors = IconButtonDefaults.iconButtonColors(
+                containerColor = if (isSaved) Color.Gray else Green500
+            )
+        ) {
+            Icon(
+                painterResource(R.drawable.filled_check),
+                contentDescription = stringResource(R.string.save_custom_view),
+                tint = Color.White
+            )
+        }
+        Spacer(Modifier.width(dimensionResource(R.dimen.padding_small)))
+        IconButton(
+            onClick = { showDeleteDialog = true },
+            shape = CircleShape,
+            colors = IconButtonDefaults.iconButtonColors(
+                containerColor = if(isSystemInDarkTheme()){
+                    MaterialTheme.colorScheme.errorContainer
+                } else {
+                    MaterialTheme.colorScheme.error
+                }
+            )
+        ) {
+            Icon(
+                painterResource(R.drawable.filled_delete),
+                contentDescription = stringResource(R.string.delete),
+                tint = if(isSystemInDarkTheme()){
+                    MaterialTheme.colorScheme.onErrorContainer
+                } else {
+                    MaterialTheme.colorScheme.onError
+                }
+            )
+        }
+    }
+}
