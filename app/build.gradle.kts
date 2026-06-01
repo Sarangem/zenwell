0bbd47371705cb439e794d4e196eb1e3ef486a96
sarangem <palproneel@gmail.com>
2026-06-01 18:23:45 +0530

    Update to v1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.sarangem.zenwell"
         minSdk = 23
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.1"
+        versionCode = 12
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
